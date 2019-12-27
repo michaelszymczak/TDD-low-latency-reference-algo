e4d51621d65ac3a7dcfd0b3564f89f16bb9a5165
Michael Szymczak <contact@michaelszymczak.com>
2019-12-27 12:45:28 +0000

    [tdd-reference-algo] Simple setup to show that MM does not do anything unprompted

--- src/main/java/com/michaelszymczak/sample/tddrefalgo/apps/middleman/MiddleManApp.java
+++ src/main/java/com/michaelszymczak/sample/tddrefalgo/apps/middleman/MiddleManApp.java
@@ -2,17 +2,39 @@ package com.michaelszymczak.sample.tddrefalgo.apps.middleman;
 
 import com.michaelszymczak.sample.tddrefalgo.framework.api.io.AppIO;
 import com.michaelszymczak.sample.tddrefalgo.framework.api.io.Output;
+import com.michaelszymczak.sample.tddrefalgo.framework.api.setup.AppFactory;
+import com.michaelszymczak.sample.tddrefalgo.framework.api.setup.AppFactoryRegistry;
+import com.michaelszymczak.sample.tddrefalgo.framework.api.setup.PayloadSchema;
+import com.michaelszymczak.sample.tddrefalgo.framework.api.setup.RegisteredAppFactory;
+import com.michaelszymczak.sample.tddrefalgo.protocols.pricing.PricingProtocolEncoding;
 import org.agrona.DirectBuffer;
+
+import java.util.Collections;
 
 public class MiddleManApp implements AppIO {
 
+    private static final PayloadSchema PRICING_SCHEMA = new PayloadSchema.KnownPayloadSchema((short) 5);
+
+    private final AppIO app;
+
+    public MiddleManApp() {
+        app = AppFactory.createApp(new AppFactoryRegistry(1024, Collections.singletonList(
+                new RegisteredAppFactory<>(
+                        PRICING_SCHEMA,
+                        new PricingProtocolEncoding.Decoder(),
+                        new PricingProtocolEncoding.Encoder(PRICING_SCHEMA),
+                        pricingMessageEncodingPublisher -> null
+                )
+        )));
+    }
+
     @Override
     public int onInput(DirectBuffer input, int offset, int length) {
-        throw new UnsupportedOperationException();
+        return 0;
     }
 
     @Override
     public Output output() {
-        throw new UnsupportedOperationException();
+        return app.output();
     }
 }
--- src/test/java/com/michaelszymczak/sample/tddrefalgo/apps/middleman/MiddleManAppPropertyTest.java
+++ src/test/java/com/michaelszymczak/sample/tddrefalgo/apps/middleman/MiddleManAppPropertyTest.java
@@ -1,13 +1,25 @@
 package com.michaelszymczak.sample.tddrefalgo.apps.middleman;
 
+import com.michaelszymczak.sample.tddrefalgo.testsupport.OutputSpy;
+import com.michaelszymczak.sample.tddrefalgo.testsupport.PricingProtocolDecodedMessageSpy;
+import org.agrona.ExpandableArrayBuffer;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 class MiddleManAppPropertyTest {
 
+    private final MiddleManApp app = new MiddleManApp();
+    private final OutputSpy<PricingProtocolDecodedMessageSpy> outputSpy = OutputSpy.outputSpy();
+
     @Test
-    @Disabled
     void shouldNotProduceSideEffectsUnprompted() {
+        app.onInput(new ExpandableArrayBuffer(), 0, 0);
+
+        outputSpy.onInput(app.output());
+
+        assertThat(outputSpy.getSpy().receivedMessages()).isEmpty();
     }
 
     @Test
