9e50f163e5594f2818e7ebf2d9a719508cb4af8e
Michael Szymczak <contact@michaelszymczak.com>
2020-01-07 08:12:14 +0000

    [tdd-reference-algo] Delegate validation

---
+++ src/main/java/com/michaelszymczak/sample/tddrefalgo/apps/middleman/domain/Tier.java
@@ -0,0 +1,9 @@
+package com.michaelszymczak.sample.tddrefalgo.apps.middleman.domain;
+
+public class Tier {
+
+    static boolean isValid(int value)
+    {
+        return value >= 1 && value <= 5;
+    }
+}
