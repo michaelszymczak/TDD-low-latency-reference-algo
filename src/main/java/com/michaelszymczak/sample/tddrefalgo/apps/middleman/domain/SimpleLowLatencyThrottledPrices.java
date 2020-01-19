20d178df53561faead3bc23721209c39b448e372
Michael Szymczak <contact@michaelszymczak.com>
2020-01-19 19:03:56 +0000

    [tdd-reference-algo] Refactor Pool

--- src/main/java/com/michaelszymczak/sample/tddrefalgo/apps/middleman/domain/SimpleLowLatencyThrottledPrices.java
+++ src/main/java/com/michaelszymczak/sample/tddrefalgo/apps/middleman/domain/SimpleLowLatencyThrottledPrices.java
@@ -5,14 +5,11 @@ import com.michaelszymczak.sample.tddrefalgo.coalescingqueue.CoalescingQueue;
 import com.michaelszymczak.sample.tddrefalgo.coalescingqueue.LowLatencyCoalescingQueue;
 import com.michaelszymczak.sample.tddrefalgo.protocols.pricing.MutableQuotePricingMessage;
 
-import java.util.ArrayDeque;
-import java.util.Deque;
 import java.util.List;
 
 public class SimpleLowLatencyThrottledPrices implements ThrottledPrices {
 
     private static final int[] POSSIBLE_TIERS;
-    private static final String KEY_DELIMITER = "/";
 
     static {
         POSSIBLE_TIERS = new int[Tier.allValues().size()];
@@ -25,13 +22,9 @@ public class SimpleLowLatencyThrottledPrices implements ThrottledPrices {
     private final ThrottledPricesPublisher publisher;
     private final int windowSize;
     private final CoalescingQueue<MutableQuotePricingMessage> queue = new LowLatencyCoalescingQueue<>();
-    private final StringBuilder keyPlaceholder = new StringBuilder();
-    private final Deque<MutableQuotePricingMessage> messagePool = new ArrayDeque<>();
     private int inFlightMessages = 0;
-    private CoalescingQueue.EvictedElementListener<MutableQuotePricingMessage> returnToThePoolEvictedMessage = message -> {
-        message.clear();
-        messagePool.addFirst(message);
-    };
+    private final Pool pool = new Pool();
+    private final CoalescingQueue.EvictedElementListener<MutableQuotePricingMessage> returnToPool = pool::returnToPool;
 
     public SimpleLowLatencyThrottledPrices(ThrottledPricesPublisher publisher, int windowSize) {
         this.publisher = publisher;
@@ -45,13 +38,7 @@ public class SimpleLowLatencyThrottledPrices implements ThrottledPrices {
 
     @Override
     public void onQuoteUpdate(CharSequence isin, int tier, long bidPrice, long askPrice) {
-        keyPlaceholder.setLength(0);
-        keyPlaceholder.append(isin).append(KEY_DELIMITER).append(tier);
-        queue.add(
-                keyPlaceholder,
-                newMessage().set(isin, tier, bidPrice, askPrice),
-                returnToThePoolEvictedMessage
-        );
+        enqueueQuote(isin, tier, bidPrice, askPrice);
         tryPublish();
     }
 
@@ -61,22 +48,28 @@ public class SimpleLowLatencyThrottledPrices implements ThrottledPrices {
         tryPublish();
     }
 
-    private void enqueueCancelledAllTiers(CharSequence isin) {
-        for (int i = 0; i < POSSIBLE_TIERS.length; i++) {
-            keyPlaceholder.setLength(0);
-            keyPlaceholder.append(isin).append(KEY_DELIMITER).append(POSSIBLE_TIERS[i]);
-            queue.add(
-                    keyPlaceholder,
-                    newMessage().set(isin, 0, 0, 0),
-                    returnToThePoolEvictedMessage
-            );
-        }
-    }
-
     @Override
     public void onAck() {
         inFlightMessages = 0;
         tryPublish();
+    }
+
+    private void enqueueQuote(CharSequence isin, int tier, long bidPrice, long askPrice) {
+        queue.add(
+                pool.reusableKey(isin, tier),
+                pool.pooledMessage(isin, tier, bidPrice, askPrice),
+                returnToPool
+        );
+    }
+
+    private void enqueueCancelledAllTiers(CharSequence isin) {
+        for (int i = 0; i < POSSIBLE_TIERS.length; i++) {
+            queue.add(
+                    pool.reusableKey(isin, POSSIBLE_TIERS[i]),
+                    pool.pooledMessage(isin, 0, 0, 0),
+                    returnToPool
+            );
+        }
     }
 
     private void tryPublish() {
@@ -98,14 +91,5 @@ public class SimpleLowLatencyThrottledPrices implements ThrottledPrices {
         return inFlightMessages >= windowSize;
     }
 
-
-    private MutableQuotePricingMessage newMessage() {
-        MutableQuotePricingMessage pooled = messagePool.pollFirst();
-        if (pooled == null) {
-            return new MutableQuotePricingMessage();
-        } else {
-            return pooled;
-        }
-    }
 
 }
